af5f4833a4906ad1850090cdb545ac3cb7050aba
heatherviolet <93415973+heatherviolet@users.noreply.github.com>
2021-11-26 18:11:32 -0800

    adding prompts

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,27 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+
+// Define password elements
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lower = "abcdefghijklmnopqrstuvwxyz";
+var numbers = "0123456789";
+var symbols = "!@#$%^&*()_+~\`|}{[]:;?><,./-=";
+
+function elementPrompt(){
+  var passLength = parseInt(prompt('How long should the password be?'));
+  if(passLength > 8 && passLength <128){
+
+  }
+}
+
+// Function to generate password
+function generatePassword(){
+  var passLength = getPassLength();
+  var charPool = getCharPool();
+  var password = buildPassword(passLength, charPool);
+  return password;
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -9,6 +30,11 @@ function writePassword() {
   passwordText.value = password;
 
 }
+// DOM elements
+const passwordTxt = document.getElementById("password");
+const length = document.getElementById("length");
+const incNumbers = document.getElementById("numbers");
+const incSymbols = document.getElementById("symbols");
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
