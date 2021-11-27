b269dd970eed95ccc00641d27008adb6fb0a94bf
heatherviolet <93415973+heatherviolet@users.noreply.github.com>
2021-11-26 19:28:28 -0800

    windows for character choice

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,44 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+// Elements in password
+var char = {
+    upper: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    lower: 'abcdefghijklmnopqrstuvwxyz',
+    numbers: '0123456789',
+    symbols: '!@#$%^&*()_+~/`|}{[]:;?><,./-='
 
-// Define password elements
-var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var lower = "abcdefghijklmnopqrstuvwxyz";
-var numbers = "0123456789";
-var symbols = "!@#$%^&*()_+~\`|}{[]:;?><,./-=";
-
-function elementPrompt(){
-  var passLength = parseInt(prompt('How long should the password be?'));
-  if(passLength > 8 && passLength <128){
-
-  }
 }
 
 // Function to generate password
-function generatePassword(){
-  var passLength = getPassLength();
-  var charPool = getCharPool();
-  var password = buildPassword(passLength, charPool);
-  return password;
-}
+function generatePassword() {
+  var passLength = parseInt(prompt('How long should the password be?'));
+  if(passLength < 8) {
+    alert('The password must be at least 8 characters');
+    return;
+  }
+  if (passLength > 128){
+    alert('The password must be less than 128 characters');
+    return;
+  }
+    var includeUpper = confirm('Would you like to include upper case letters?');
+    var includeLower = confirm('Would you like to include lower case letters?');
+    var includeNumbers = confirm('Would you like to include numbers?');
+    var includeSymbols = confirm('Would you like to include symbols?');
+
+    if (!includeUpper && !includeLower && !includeNumbers && !includeSymbols){
+      alert('You must choose at least one uppercase letter, lowercase letter, number or symbol character');
+      return;
+    }
+  }
+
+
+  //var passLength = getPassLength();
+  //var charPool = getCharPool();
+  //var password = buildPassword(passLength, charPool);
+  //return password;
+//};
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
