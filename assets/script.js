6ccbfda0456c7b8815ce8122c2b21828191b0f1a
heatherviolet <93415973+heatherviolet@users.noreply.github.com>
2021-11-26 22:47:11 -0800

    code redo

--- assets/script.js
+++ assets/script.js
@@ -1,57 +1,32 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Elements in password
-var char = {
-    upper: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-    lower: 'abcdefghijklmnopqrstuvwxyz',
-    numbers: '0123456789',
-    symbols: '!@#$%^&*()_+~/`|}{[]:;?><,./-='
-
-}
-
-// Function to generate password
 function generatePassword() {
-  var passLength = parseInt(prompt('How long should the password be?'));
-  if(passLength < 8) {
-    alert('The password must be at least 8 characters');
-    return;
-  }
-  if (passLength > 128){
-    alert('The password must be less than 128 characters');
-    return;
-  }
-    var includeUpper = confirm('Would you like to include upper case letters?');
-    var includeLower = confirm('Would you like to include lower case letters?');
-    var includeNumbers = confirm('Would you like to include numbers?');
-    var includeSymbols = confirm('Would you like to include symbols?');
 
-    if (!includeUpper && !includeLower && !includeNumbers && !includeSymbols){
-      alert('You must choose at least one uppercase letter, lowercase letter, number or symbol character');
-      return;
-    }
-  }
-
-
-  //var passLength = getPassLength();
-  //var charPool = getCharPool();
-  //var password = buildPassword(passLength, charPool);
-  //return password;
-//};
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+var passLength = parseInt(prompt('How long should the password be?')); 
+if (passLength < 8) {
+  alert('The password must be at least 8 characters!');
+  return;
 }
-// DOM elements
-const passwordTxt = document.getElementById("password");
-const length = document.getElementById("length");
-const incNumbers = document.getElementById("numbers");
-const incSymbols = document.getElementById("symbols");
+if (passLength > 128) {
+  alert('The password must be less than 128 characters!');
+  return;
+}
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numbArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var characterArray = ["@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
+
+var resultArray = [];
+var userArray = [];
+
+uppercaseArray [1]
+  
+
+  var numbers = confirm ("Would you like numbers in the password?");
+  var uppercases = confirm ("Would you like uppercase letters in the password?");
+  var lowercases = confirm ("Would you like lower case letters in the password?");
+  var characters = confirm ("Would you like symbols in the password?");
+
+
